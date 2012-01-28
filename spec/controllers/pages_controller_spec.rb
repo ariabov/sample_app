b5bda289d1660b314ce16f61c09e1c489f85110c
Alex Riabov <spralex@gmail.com>
2012-01-27 21:35:05 -0800

    Stylesheets and helpers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views #the code to render pages for the test enginer to see
   
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-      :content => "Ruby on Rails Tutorial Sample App | Home")
+      :content => "#{base_title} | Home")
     end
     it "should have a non-blank body" do
       get 'home'
@@ -27,7 +31,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-      :content => "Ruby on Rails Tutorial Sample App | Contact")
+      :content => "#{base_title} | Home")
     end
   end
 
@@ -39,7 +43,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-      :content => "Ruby on Rails Tutorial Sample App | About")
+      :content => "#{base_title} | Home")
     end
   end
 end

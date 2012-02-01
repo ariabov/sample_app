163f168f57c898fe33722bb1d6a56d6be4a21fb1
Alex Riabov <spralex@gmail.com>
2012-02-01 11:48:21 -0800

    Added layout links (with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -46,4 +46,16 @@ describe PagesController do
       :content => "#{base_title} | Home")
     end
   end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+      :content => "#{base_title} | Home")
+    end
+  end
 end
